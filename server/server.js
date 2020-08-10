940f042b9bb3f2e09994149ad927fbdea71bddd4
buckhalton <isaiah.buckhalton@gmail.com>
2020-08-10 00:18:39 -0500

    Created server foundation and tested API

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
 
@@ -8,8 +10,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
-app.get('/ping', function (req, res) {
-    return res.send('pong');
+app.get('/genres', function (req, res) {
+    axios({
+        method: 'GET',
+        url: 'https://unogs-unogs-v1.p.rapidapi.com/api.cgi',
+        headers: {
+            'content-type': 'application/octet-stream',
+            'x-rapidapi-host': 'unogs-unogs-v1.p.rapidapi.com',
+            'x-rapidapi-key': process.env.UNOGS_API_KEY,
+            useQueryString: true,
+        },
+        params: {
+            t: 'genres',
+        },
+    })
+        .then((response) => {
+            res.send(response.data);
+        })
+        .catch((error) => {
+            res.send(error);
+            console.error(error);
+        });
 });
 
 // Serve static files
--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const handleClick = () => {
+        axios
+            .get('/ping')
+            .then((res) => {
+                console.log('response:', res);
+            })
+            .catch((err) => {
+                console.log('error:', err);
+            });
+    };
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                    <button onClick={handleClick}>Click on me</button>
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
