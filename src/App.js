930840cb881e9604204958a00fba2ab7d61df381
buckhalton <isaiah.buckhalton@gmail.com>
2020-08-25 17:43:44 -0500

    Testing lint-staged

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
                                         'darkBlue',
                                     ]}
                                 >
-                                    genres
+                                    Genres
                                 </Box>
                             </div>
                         </Route>
