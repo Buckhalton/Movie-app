0fdb4eeaa142d98d9b32bd06c41240af9cfd8294
buckhalton <isaiah.buckhalton@gmail.com>
2020-08-25 17:05:37 -0500

    Added cross-env

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
                                         'darkBlue',
                                     ]}
                                 >
-                                    genres
+                                    test
                                 </Box>
                             </div>
                         </Route>
